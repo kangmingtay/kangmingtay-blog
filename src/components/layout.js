6a4d8a475da96e81cd95dae85f48c58b30958616
kangmingtay <kang.ming1996@gmail.com>
2020-05-25 19:15:02 +0800

    refactor styles in layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,76 +4,41 @@ import { Link } from "gatsby"
 import { rhythm } from "../utils/typography"
 
 const Layout = ({ location, title, children }) => {
-  // const rootPath = `${__PATH_PREFIX__}/`
-  let header =  (
-    <h3
-      style={{
-        fontFamily: `Inconsolata`,
-        marginTop: 0,
-      }}
-    >
-      <ul style={{
-          listStyle: 'none',
-          display: 'flex',
-          justifyContent: 'space-evenly',
-      }}>
-          <li><Link style={{ boxShadow: `none`, color: `inherit`}} to={`/`}>{title}</Link></li>
-          <li><Link style={{ boxShadow: `none`, color: `inherit`}} to="/about">About</Link></li>
-          <li><Link style={{ boxShadow: `none`, color: `inherit`}} to="/blog">Blog</Link></li>
+
+  const styles = {
+    h3: {
+      fontFamily: `Inconsolata`,
+      marginTop: 0,
+    },
+    div: {
+      marginLeft: `auto`,
+      marginRight: `auto`,
+      maxWidth: rhythm(30),
+      padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+    },
+    ul: {
+      listStyle: 'none',
+      display: 'flex',
+      justifyContent: 'space-evenly',
+    },
+    link: {
+      boxShadow: `none`, 
+      color: `inherit`
+    }
+  }
+
+  const header =  (
+    <h3 style={styles.h3}>
+      <ul style={styles.ul}>
+          <li><Link style={styles.link} to={`/`}>{title}</Link></li>
+          <li><Link style={styles.link} to="/about">About</Link></li>
+          <li><Link style={styles.link} to="/blog">Blog</Link></li>
       </ul>
     </h3>
   )
 
-  // if (location.pathname === rootPath) {
-  //   header = (
-  //     <h1
-  //       style={{
-  //         ...scale(1.5),
-  //         marginBottom: rhythm(1.5),
-  //         marginTop: 0,
-  //         textAlign: 'center',
-  //       }}
-  //     >
-  //       <Link
-  //         style={{
-  //           boxShadow: `none`,
-  //           color: `inherit`,
-  //         }}
-  //         to={`/`}
-  //       >
-  //         {title}
-  //       </Link>
-  //     </h1>
-  //   )
-  // } else {
-  //   header = (
-  //     <h3
-  //       style={{
-  //         fontFamily: `Inconsolata`,
-  //         marginTop: 0,
-  //       }}
-  //     >
-  //       <Link
-  //         style={{
-  //           boxShadow: `none`,
-  //           color: `inherit`,
-  //         }}
-  //         to={`/`}
-  //       >
-  //         {title}
-  //       </Link>
-  //     </h3>
-  //   )
-  // }
   return (
-    <div
-      style={{
-        marginLeft: `auto`,
-        marginRight: `auto`,
-        maxWidth: rhythm(30),
-        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-      }}
-    >
+    <div style={styles.div}>
       <header>{header}</header>
       <main>{children}</main>
       <footer>
